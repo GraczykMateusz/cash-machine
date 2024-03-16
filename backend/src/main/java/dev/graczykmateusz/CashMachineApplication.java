ecc44ea72f5104a47e411b920387766ece70a7e1
Mateusz Graczyk <dev.graczykmateusz@gmail.com>
2024-03-16 23:12:11 +0100

    building to one jar

---
+++ backend/src/main/java/dev/graczykmateusz/CashMachineApplication.java
@@ -0,0 +1,12 @@
+package dev.graczykmateusz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashMachineApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CashMachineApplication.class);
+  }
+}
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,12 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'cash-machine';
+  
+  private readonly http = inject(HttpClient);
+  
+  r: any;
+  
+  test() {
+    this.http.get("/api/test").pipe(take(1)).subscribe(r => this.r = r)
+  }
 }
