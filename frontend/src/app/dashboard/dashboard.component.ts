4e609f9f5bb4a32cc102d5839bb206defa9fabbb
Mateusz Graczyk <dev.graczykmateusz@gmail.com>
2024-03-24 15:46:18 +0100

    [fe] eng/pl flags

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    NgbNavModule,
+    NgOptimizedImage
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
-
+  
+  username: string = 'Mateusz';
+  balance: number = 100;
+  currency: string = 'PLN';
 }
