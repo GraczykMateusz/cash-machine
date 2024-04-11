d88ea822ed2dbb7cd5d4a52ca99098e0e7a1a474
Mateusz Graczyk <dev.graczykmateusz@gmail.com>
2024-04-11 21:56:52 +0200

    [fe] forex chart draft

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -5,10 +5,12 @@ import { routes } from './routing/app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
 import '@angular/common/locales/global/pl';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
+    provideCharts(withDefaultRegisterables()),
     provideHttpClient(),
     {
       provide: LOCALE_ID,
--- frontend/src/app/forex/forex.component.ts
+++ frontend/src/app/forex/forex.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { ChartConfiguration, ChartOptions } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-forex',
   standalone: true,
-  imports: [],
+  imports: [
+    BaseChartDirective
+  ],
   templateUrl: './forex.component.html',
   styleUrl: './forex.component.scss'
 })
 export class ForexComponent {
-
+  
+  title = 'ng2-charts-demo';
+  
+  public lineChartData: ChartConfiguration<'line'>['data'] = {
+    labels: [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July'
+    ],
+    datasets: [
+      {
+        data: [ 65, 59, 80, 81, 56, 55, 40 ],
+        label: 'Series A',
+        fill: true,
+        tension: 0.5,
+        borderColor: 'black',
+        backgroundColor: 'rgba(255,0,0,0.3)'
+      }
+    ]
+  };
+  public lineChartOptions: ChartOptions<'line'> = {
+    responsive: true,
+    maintainAspectRatio: false,
+  };
+  public lineChartLegend = true;
+  
 }
